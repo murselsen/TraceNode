50d263d9cfa1acacd1868a4caf3b779d239c178b
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-04-02 03:46:54 +0300

    Add AddModel component for app model display

---
+++ app/(tabs)/settings/add-model.tsx
@@ -0,0 +1,19 @@
+import ScreenLayout from "@/components/ScreenLayout";
+import LinkButton from "@/components/ui/LinkButton";
+import React from "react";
+import { Text, View } from "react-native";
+
+const AddModel = () => {
+  return (
+    <>
+      <ScreenLayout>
+        <View style={{ flexShrink: 1 }}>
+          <Text>App Model </Text>
+         
+        </View>
+      </ScreenLayout>
+    </>
+  );
+};
+
+export default AddModel;
