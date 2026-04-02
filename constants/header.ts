f6a1fa17cdd6b4b2bdabc2cabfbb61dc75acdb33
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-04-02 03:47:19 +0300

    Add header and redux constants for styling and state management

---
+++ constants/header.ts
@@ -0,0 +1,4 @@
+export const style = { backgroundColor: "#2CB4ED" };
+export const titleColor = "#fff";
+export const titleStyle = { fontSize: 20, fontWeight: "bold" };
+export const shadowVisible = false;
---
+++ constants/redux.ts
@@ -0,0 +1,10 @@
+const slices = {
+  auth: {
+    name: "auth",
+    paths: {
+      slice: "@/redux/Auth/slice",
+      selector: "@/redux/Auth/selector",
+      thunk: "@/redux/Auth/thunk",
+    },
+  },
+};
